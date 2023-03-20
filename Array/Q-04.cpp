51f92a29456eb23f67ec157f1a8ef6332dfefe55
Neelesh <neeleshsingh2072003@gmail.com>
2023-03-21 00:01:37 +0530

    Array Quetions Update

---
+++ Array/Q-04.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10] = {1,2,3,4,6,7,8,9,10};
+    for(int i=0;i<10; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+} 
+
---
+++ Array/Q-06.cpp
@@ -0,0 +1,18 @@
+// Take 5 elements as an input in integer array and print the 2x Value(Double of value).
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    cout<<"Enter 5 elements ";
+    for(int i=0; i<5; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<5; i++)
+    {
+        cout<<arr[i]*2<<" ";
+    }
+}
