1dcf39e7c777b193c6bcf027bf92becdf1b5f0a5
Neelesh <neeleshsingh2072003@gmail.com>
2023-07-02 21:03:29 +0530

    Sorting and Searching

--- 04_Sorting and Searching/Q-04_firstOccurrenceOfAnElement.cpp
+++ 04_Sorting and Searching/Q-04_firstOccurrenceOfAnElement.cpp
@@ -1,15 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int firstOccurrence(vector<int>array, int target){
+pair<int, int> firstOccurrence(vector<int>array, int target){
+     pair<int , int>p;
     int start = 0;
-    int end = array.size()-1;
-    int ans = -1;
+    int end = array.size()-1;   
+    int f = -1;
+    int l=-1;
     while(start <= end){
         int mid = start + (end - start)/2;
         if(array[mid] == target){
-            ans = mid;
+            f = mid;
             end  = mid-1;
+        }
+      
+        if(array[mid] < target){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+
+        while(start <= end){
+        int mid = start + (end - start)/2;
+       
+         if(array[mid] == target){
+            l = mid;
+            end  = mid+1;
         }
         if(array[mid] < target){
             start = mid+1;
@@ -18,7 +35,9 @@ int firstOccurrence(vector<int>array, int target){
             end = mid-1;
         }
     }
-    return ans;
+    p.first = f;
+    p.second = l;
+    return p;
 }
 // Take input
 vector<int> takeInput( int size){
@@ -31,6 +50,7 @@ vector<int> takeInput( int size){
    return arr;
 }
 int main(){
+    pair<int , int>p;
     int n;
     cout<<"Enter the size of an array: "<<endl;
     cin>>n;
@@ -40,9 +60,16 @@ int main(){
     int target;
     cout<<"Enter a target element: "<<endl;
     cin>>target;
-    int index = firstOccurrence(array,target);
-    if(index != -1)
-    cout<<"First Occurrence of "<<target<<" is at "<<index<<" index."<<endl;
+    p = firstOccurrence(array,target);
+    if(p.first != -1){
+        cout<<"First Occurrence of "<<target<<" is at "<<p.first<<" index."<<endl;
+       
+    }
+    if(p.second != -1){
+        
+        cout<<"Last Occurrence of "<<target<<" is at "<<p.second<<" index."<<endl;
+    }
+
     else
     cout<<"Element is not present in array.";
 }
